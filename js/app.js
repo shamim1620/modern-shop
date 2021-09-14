29163a0dbd2d1622e98d44222c97dcfa25d82426
Shamim Ahmed <cse.shamimahmed@gmail.com>
2021-09-14 14:43:05 +0600

    change padding

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    // console.log(product.rating.rate);
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
